1491dbe1ef95751f4c61baf362b29b270f341cfe
Naoki <knk43nok@gmail.com>
2019-07-02 21:41:23 +0900

    modify application.rb

--- config/application.rb
+++ config/application.rb
@@ -27,8 +27,24 @@ module QiitaClone
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
+    # Don't generate system test files.
+    config.generators.system_tests = nil
+
     config.generators do |g|
-      g.test_framework :rspec
+      g.template_engine false
+      g.javascripts false
+      g.stylesheets false
+      g.helper false
+      g.test_framework :rspec,
+                       fixtures: true,
+                       fixture_replacement: :factory_bot,
+                       view_specs: false,
+                       routing_specs: false,
+                       helper_specs: false,
+                       controller_specs: false,
+                       request_specs: true
     end
+
+    config.api_only = true
   end
 end
