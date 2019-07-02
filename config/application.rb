daa0cdbfd78fc81e360829cf3843fffbf9bd8a0b
Naoki <knk43nok@gmail.com>
2019-07-02 20:27:08 +0900

    build the environment for the test

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,8 @@ module QiitaClone
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
-    # Don't generate system test files.
-    config.generators.system_tests = nil
+    config.generators do |g|
+      g.test_framework :rspec
+    end
   end
 end
