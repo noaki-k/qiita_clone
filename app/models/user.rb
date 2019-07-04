19d8da33a15097c92d76086d2ddf58868cb65b5b
Naoki <knk43nok@gmail.com>
2019-07-04 21:18:19 +0900

    Userモデルのvalidationと他モデルとのリレーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,26 @@
 class User < ApplicationRecord
+
+  #nameへのvalidationを設定
+  validates :name, presence: true
+  validates :name, length: { maximum: 15 }
+
+  # emailへのvalidationを設定
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+
+  # passwordへのvalidationを設定
+  validates :password, presence: true
+  validates :password, uniqueness: true
+  validates :password, length: { maximum: 15 }
+
+  #Userと他モデルのリレーションを記述
+  has_one :user_detail
+  has_many :articles
+  has_many :article_likes
+  has_many :comments
+  has_many :comment_likes
+
+
 end
