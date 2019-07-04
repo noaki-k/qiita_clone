b280e51831dbe37b3baf30974509bb03c64cd214
Naoki <knk43nok@gmail.com>
2019-07-04 23:43:25 +0900

    UserDetailモデルのvalidationを追加

--- app/models/user_detail.rb
+++ app/models/user_detail.rb
@@ -1,3 +1,6 @@
 class UserDetail < ApplicationRecord
   belongs_to :user
+  #avatarのvalidationを設定
+  validates :avatar, file_size: { less_than_or_equal_to: 100.kilobytes }
+  validates :avatar,   file_content_type: { allow: ['image/jpeg', 'image/png'] }
 end
