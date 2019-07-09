608da5ad000e730cf30d40fe8b7274d72677f680
Naoki <knk43nok@gmail.com>
2019-07-09 19:59:30 +0900

    implement user relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   #:confirmable, :omniauthable
   include DeviseTokenAuth::Concerns::User
+
+  #Userと他モデルのリレーションを記述
+  has_one :user_detail
+  has_many :articles
+  has_many :article_likes
+  has_many :comments
+  has_many :comment_likes
 end
