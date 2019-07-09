3e57ba59a264204ed4b4ec309e41d0b807e2dce1
Naoki <knk43nok@gmail.com>
2019-07-09 19:14:16 +0900

    modify files following the advice2

--- app/controllers/api/auth/registrations_controller.rb
+++ app/controllers/api/auth/registrations_controller.rb
@@ -1,17 +1,16 @@
 module Api
   module Auth
-    class RegistrationsController < DeviseTokenAuth::RegistrationsController
-
+    class Api::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
       private
+
       # :passwordを追加できるようにpravateメソッドに修正を加える
       def sign_up_params
-        params.permit(:name, :email,  :password, :password_confirmation)
+        params.permit(:name, :email, :password, :password_confirmation)
       end
 
       def account_update_params
         params.permit(:name, :email, :password)
       end
-
     end
   end
 end
