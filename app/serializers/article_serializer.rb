6ecdb78a907930cdc4cc732548903119f0c41f79
Naoki <knk43nok@gmail.com>
2019-07-10 23:46:46 +0900

    add associations

--- app/serializers/article_serializer.rb
+++ app/serializers/article_serializer.rb
@@ -1,3 +1,7 @@
 class ArticleSerializer < ActiveModel::Serializer
   attributes :id
+
+  belongs_to :user
+  has_many :article_likes, dependent: :destroy
+  has_many :comment, dependent: :destroy
 end
