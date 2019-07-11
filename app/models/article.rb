9f4b4cbbe0a169cfe39c165038b861a236ea4813
Naoki <knk43nok@gmail.com>
2019-07-11 20:26:14 +0900

    add validation to article.rb

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,7 @@ class Article < ApplicationRecord
   belongs_to :user
   has_many :article_likes, dependent: :destroy
   has_many :comment, dependent: :destroy
+
+  validates :title, presence: true
+  validates :text, presence: true
 end
