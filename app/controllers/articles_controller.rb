e5872d87e13f18ad54a0c88e265477d484b1e905
Naoki <knk43nok@gmail.com>
2019-07-13 21:07:18 +0900

    implement API for UPDATE

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: [:show]
+  before_action :set_article, only: [:show, :update]
 
   def index
     @articles = Article.all
@@ -10,9 +10,18 @@ class ArticlesController < ApplicationController
     render json: @article
   end
 
+  def update
+    @article.update!(article_params)
+    render json: @article
+  end
+
   private
 
   def set_article
     @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
 end
