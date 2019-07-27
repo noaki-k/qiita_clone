6a8deae10997f7f84971c0b886f879a8653488be
Naoki <knk43nok@gmail.com>
2019-07-27 20:30:30 +0900

    add alias and modify application controller

--- app/controllers/api/v1/base_api_controller.rb
+++ app/controllers/api/v1/base_api_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::BaseApiController < ApplicationController
   include DeviseTokenAuth::Concerns::SetUserByToken
-  # FIXME: devise_token_auth の current_user を導入するまでのダミーコード
-  def current_user
-    @current_user ||= User.first
-  end
+
+  alias_method :current_user, :current_api_v1_user
+  alias_method :authenticate_user!, :authenticate_api_v1_user!
+  alias_method :user_signed_in?, :api_v1_user_signed_in?
 end
