1bb21d31809a8d0845afafaa4d8a5cb3574c89ce
Naoki <knk43nok@gmail.com>
2019-07-31 07:50:06 +0900

    turn off CSRF

--- app/controllers/api/v1/base_api_controller.rb
+++ app/controllers/api/v1/base_api_controller.rb
@@ -4,4 +4,6 @@ class Api::V1::BaseApiController < ApplicationController
   alias_method :current_user, :current_api_v1_user
   alias_method :authenticate_user!, :authenticate_api_v1_user!
   alias_method :user_signed_in?, :api_v1_user_signed_in?
+
+  protect_from_forgery with: :null_session
 end
