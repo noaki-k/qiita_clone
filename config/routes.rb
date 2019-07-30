45dfb61e04cb6f914ec3d400cec53d0c36902ca9
Naoki <knk43nok@gmail.com>
2019-07-30 20:03:59 +0900

    Add sign_in page

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,16 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'homes#index'
 
+  # reload 対策
   get "sign_up", to: "homes#index"
+  get 'sign_in', to: 'homes#index'
 
   namespace :api, format: :json do
     namespace :v1 do
       mount_devise_token_auth_for 'User', at: 'auth', controllers: {
         registrations: 'api/v1/auth/registrations',
         sessions: 'api/v1/auth/sessions'
-    }
+      }
       resources :articles
     end
   end
