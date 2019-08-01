79ad923f82517c0abb4a4e6cbda7be2d3e019f1b
Naoki <knk43nok@gmail.com>
2019-08-01 18:57:30 +0900

    fix top page and others layout

--- app/javascript/packs/container/ArticlesContainer.vue
+++ app/javascript/packs/container/ArticlesContainer.vue
@@ -1,11 +1,21 @@
 <template>
-<div id="articles-container">
-    <ul>
-      <li v-for="article in articles" v-bind:key="article.id">
-        <div>{{article.title}}</div>
-        <div>{{article.text}}</div>
-      </li>
-    </ul>
+<v-container class="item elevation-3 articles-container">
+    <v-list two-line>
+      <template v-for="(article, index) in articles">
+        <v-list-tile :key="article.title" avatar>
+          <v-list-tile-avatar>
+            <img :src="article.avatar">
+          </v-list-tile-avatar>
+
+          <v-list-tile-content>
+            <v-list-tile-title>{{ article.title }}</v-list-tile-title>
+            <v-list-tile-sub-title>by {{ article.user.name }} {{ article.updated_at }}</v-list-tile-sub-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-divider :key="index"></v-divider>
+      </template>
+    </v-list>
+  </v-container>
   </div>
 </template>
 
@@ -27,3 +37,9 @@ export default class ArticlesContainer extends Vue {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.articles-container {
+  margin-top: 2em;
+}
+</style>
--- app/javascript/packs/container/RegisterContainer.vue
+++ app/javascript/packs/container/RegisterContainer.vue
@@ -1,37 +1,39 @@
 <template>
-  <form>
-    <v-text-field
-      v-model="name"
-      v-validate="'required|max:10'"
-      :counter="10"
-      :error-messages="errors.collect('name')"
-      label="ユーザー名"
-      data-vv-name="name"
-      required
-    ></v-text-field>
-    <v-text-field
-      v-model="email"
-      v-validate="'required|email'"
-      :error-messages="errors.collect('email')"
-      label="メールアドレス"
-      data-vv-name="email"
-      required
-    ></v-text-field>
-    <v-text-field
-      v-model="password"
-      :append-icon="show ? 'visibility' : 'visibility_off'"
-      :type="show ? 'text' : 'password'"
-      v-validate="'required|min:8|max:50'"
-      :error-messages="errors.collect('password')"
-      name="password"
-      label="パスワード"
-      hint="At least 8 characters"
-      counter
-      @click:append="show = !show"
-    ></v-text-field>
+  <v-container>
+    <form>
+      <v-text-field
+        v-model="name"
+        v-validate="'required|max:10'"
+        :counter="10"
+        :error-messages="errors.collect('name')"
+        label="ユーザー名"
+        data-vv-name="name"
+        required
+      ></v-text-field>
+      <v-text-field
+        v-model="email"
+        v-validate="'required|email'"
+        :error-messages="errors.collect('email')"
+        label="メールアドレス"
+        data-vv-name="email"
+        required
+      ></v-text-field>
+      <v-text-field
+        v-model="password"
+        :append-icon="show ? 'visibility' : 'visibility_off'"
+        :type="show ? 'text' : 'password'"
+        v-validate="'required|min:8|max:50'"
+        :error-messages="errors.collect('password')"
+        name="password"
+        label="パスワード"
+        hint="At least 8 characters"
+        counter
+        @click:append="show = !show"
+      ></v-text-field>
 
-    <v-btn @click="submit" color="#55c500" class="white--text font-weight-bold">登録</v-btn>
-  </form>
+      <v-btn @click="submit" color="#55c500" class="white--text font-weight-bold">登録</v-btn>
+    </form>
+  </v-container>
 </template>
 
 <script lang="ts">
