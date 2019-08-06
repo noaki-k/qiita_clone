87053bc96a52a5ae261966c46fb938d8abf6320a
Naoki <knk43nok@gmail.com>
2019-08-06 21:48:45 +0900

    change matchers and integrate the expression in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,11 @@ Rails.application.routes.draw do
   root 'homes#index'
 
   # reload 対策
-  get "sign_up", to: "homes#index"
+  get 'sign_up', to: 'homes#index'
   get 'sign_in', to: 'homes#index'
-  get "articles/new", to: "homes#index"
-  get "articles/:id", to: "homes#index"
-  get "mypage", to: "homes#index"
-
+  get 'articles/new', to: 'homes#index'
+  get 'articles/:id', to: 'homes#index'
+  get 'mypage', to: 'homes#index'
 
   namespace :api, format: :json do
     namespace :v1 do
@@ -16,7 +15,7 @@ Rails.application.routes.draw do
         registrations: 'api/v1/auth/registrations',
         sessions: 'api/v1/auth/sessions'
       }
-      get '/mypage' => 'mypage#index'
+      get '/mypage', to: 'mypage#index'
       resources :articles
     end
   end
