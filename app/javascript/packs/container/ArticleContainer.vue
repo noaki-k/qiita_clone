54e315353b78a67dfc337f1dcce5676a66fbcc9b
Naoki <knk43nok@gmail.com>
2019-08-15 15:09:01 +0900

    add update function

--- app/javascript/packs/container/ArticleContainer.vue
+++ app/javascript/packs/container/ArticleContainer.vue
@@ -3,6 +3,21 @@
     <v-layout xs-12 class="top-info-container">
       <span class="user-name">@{{ article.user.name }}</span>
       <time-ago :refresh="60" :datetime="article.updated_at" locale="en" tooltip="top" long></time-ago>
+      <v-spacer></v-spacer>
+      <v-btn
+        fab
+        flat
+        dark
+        small
+        color="#55c500"
+        @click="moveToEditArticlePage(article.id)"
+        v-if="editable"
+      >
+        <v-icon dark>edit</v-icon>
+      </v-btn>
+      <v-btn fab flat dark small color="#55c500">
+        <v-icon dark>list</v-icon>
+      </v-btn>
     </v-layout>
     <v-layout>
       <h1 class="article-title">{{ article.title }}</h1>
@@ -17,6 +32,7 @@
 import axios from "axios";
 import { Vue, Component } from "vue-property-decorator";
 import TimeAgo from "vue2-timeago";
+import Router from "../router/router";
 @Component({
   components: {
     TimeAgo
@@ -26,6 +42,10 @@ export default class ArticleContainer extends Vue {
   article: any = "";
   async mounted(): Promise<void> {
     await this.fetchArticle(this.$route.params.id);
+  }
+
+  get editable(): boolean {
+    return localStorage.getItem("uid") === this.article.user.email;
   }
   async fetchArticle(id: string): Promise<void> {
     await axios
@@ -37,6 +57,9 @@ export default class ArticleContainer extends Vue {
         // TODO: 適切な Error 表示
         alert(e.response.statusText);
       });
+  }
+  moveToEditArticlePage(id: string): void {
+    Router.push(`/articles/${id}/edit`);
   }
 }
 </script>
